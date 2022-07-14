c1dd480f7cfdeb24e280d1f429c1c92f28b13464
Torres-ssf <sergio.uft@gmail.com>
2022-07-14 13:59:03 -0300

    implement global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle, css } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']};
+  }
+
+  body {
+    ${({ theme }) => css`
+      background: ${theme['gray-900']};
+      color: ${theme['gray-300']};
+    `};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`
