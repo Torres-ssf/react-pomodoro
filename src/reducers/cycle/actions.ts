c778fea9ce7eda3b286461f77fd7d0c5d76716a1
Torres-ssf <sergio.uft@gmail.com>
2022-07-20 20:50:47 -0300

    add actions for Cycle reducer

---
+++ src/reducers/cycle/actions.ts
@@ -0,0 +1,28 @@
+import { ICycle } from '../../contexts/CycleContext'
+
+export enum CycleReducerActionTypeEnum {
+  ADD_NEW_CYCLE = 'ADD_NEW_CYCLE',
+  MARK_CURRENT_CYCLE_AS_FINISHED = 'MARK_CURRENT_CYCLE_AS_FINISHED',
+  INTERRUPT_CURRENT_CYCLE = 'INTERRUPT_CURRENT_CYCLE',
+}
+
+export function addNewCycleAction(newCycle: ICycle) {
+  return {
+    type: CycleReducerActionTypeEnum.ADD_NEW_CYCLE,
+    payload: {
+      newCycle,
+    },
+  }
+}
+
+export function interruptCurrentCycleAction() {
+  return {
+    type: CycleReducerActionTypeEnum.ADD_NEW_CYCLE,
+  }
+}
+
+export function markCurrentCycleAsFinishedAction() {
+  return {
+    type: CycleReducerActionTypeEnum.ADD_NEW_CYCLE,
+  }
+}
