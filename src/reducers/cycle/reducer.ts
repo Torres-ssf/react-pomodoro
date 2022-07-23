a880dd76dd3ba311ef51dd131edbc69904db4962
Torres-ssf <sergio.uft@gmail.com>
2022-07-23 12:46:27 -0300

    add cycle reducer functionality to mark cycle as finished to false

--- src/reducers/cycle/reducer.ts
+++ src/reducers/cycle/reducer.ts
@@ -4,6 +4,7 @@ import { CycleReducerActionTypeEnum } from './actions'
 export interface ICycleState {
   cycles: ICycle[]
   activeCycleId: string | undefined
+  isCycleFinished: boolean
 }
 
 interface ICycleReducerAction {
@@ -18,11 +19,13 @@ export function cycleReducer(
   switch (action.type) {
     case CycleReducerActionTypeEnum.ADD_NEW_CYCLE:
       return {
+        ...state,
         cycles: [...state.cycles, action.payload.newCycle],
         activeCycleId: action.payload.newCycle.id,
       }
     case CycleReducerActionTypeEnum.INTERRUPT_CURRENT_CYCLE:
       return {
+        ...state,
         cycles: state.cycles.map(cycle => {
           if (cycle.id === state.activeCycleId) {
             return {
@@ -46,6 +49,12 @@ export function cycleReducer(
           return cycle
         }),
         activeCycleId: undefined,
+        isCycleFinished: true,
+      }
+    case CycleReducerActionTypeEnum.SET_CYCLE_AS_NOT_FINISHED:
+      return {
+        ...state,
+        isCycleFinished: false,
       }
     default:
       return state
